a3f3f9be19aa9daf7b31a3f96e4b6b1c43b151be
Shubham Khetan <12shubhamkhetan@gmail.com>
2021-09-29 18:33:54 +0530

    Initial Commit

---
+++ src/main/java/com/temedica/coding/challenge/TemedicaCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.temedica.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemedicaCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemedicaCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/temedica/coding/challenge/TemedicaCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temedica.coding.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemedicaCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
